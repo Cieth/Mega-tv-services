f4753e29a8590b2dfd279b9410cef18fd4757d5f
Omar Vega <omarvega598@gmail.com>
2022-12-09 20:02:51 -0500

    add basic page

--- src/pages/Basics.tsx
+++ src/pages/Basics.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import { useState } from 'react';
 import '../styles/pages/Basics.scss';
+import { ChangeEvent } from 'react';
 import { Link } from 'react-router-dom';
 const Basics = () => {
+  interface dataI {
+    name?: string;
+    lastName?: string;
+  }
+  const [data, setData] = useState<dataI>({});
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.target.name === 'name'
+      ? setData({ ...data, name: e.target.value })
+      : setData({ ...data, lastName: e.target.value });
+  };
+  console.log(data);
   return (
     <div className='Basics'>
       <div className='Basics_text'>
         <p>Awesome! Let's get started with the basics.</p>
       </div>
       <div className='Basics_input'>
-        <input placeholder='FIRST NAME' type='text' />
-        <input placeholder='LAST NAME' type='text' />
+        <input
+          onChange={handleChange}
+          value={data.name}
+          name={'name'}
+          placeholder='First name'
+          type='text'
+        />
+        <input
+          onChange={handleChange}
+          value={data.lastName}
+          name={'lastName'}
+          placeholder='Last name'
+          type='text'
+        />
       </div>
       <div className='Basics_button'>
         <Link to={'/basics'}>
