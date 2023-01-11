86459a4521fb512b04120c4db0b28f9404234626
Omar Vega <omarvega598@gmail.com>
2023-01-10 23:54:25 -0500

    add final message

--- src/pages/FinalSteps.tsx
+++ src/pages/FinalSteps.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const FinalSteps = () => {
-  return <div>FinalSteps</div>;
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%,-50%)',
+      }}
+    >
+      You're all finished
+    </div>
+  );
 };
 
 export default FinalSteps;
