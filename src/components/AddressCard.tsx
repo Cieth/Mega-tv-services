6b6d5c6b205b1472c40c15288b5a7822a7918564
Omar Vega <omarvega598@gmail.com>
2023-01-11 21:55:25 -0500

    add styles

--- src/components/AddressCard.tsx
+++ src/components/AddressCard.tsx
@@ -1,5 +1,5 @@
 import { useDispatch } from 'react-redux';
-
+import '../styles/components/AdressCard.scss';
 import { setAddress, setIdBuilding } from '../features/slices/userSlice';
 import { IBuilding } from '../types/IBuilding';
 const AddressCard = ({ _id, name, address, state, postal }: IBuilding) => {
@@ -10,20 +10,12 @@ const AddressCard = ({ _id, name, address, state, postal }: IBuilding) => {
     dispatch(setIdBuilding(_id));
   };
   return (
-    <div
-      style={{
-        display: 'flex',
-        margin: '1rem',
-        columnGap: '10rem',
-        backgroundColor: '#ccc',
-        borderRadius: '1rem',
-      }}
-    >
-      <div style={{}}>
+    <div className='CardBody'>
+      <div className='CardNames'>
         <div>{name}</div>
         <div>{`${address},${state},${postal}`}</div>
       </div>
-      <div>
+      <div className='CardButton'>
         <button onClick={handleClick}>SELECT</button>
       </div>
     </div>
